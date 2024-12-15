4a0c36b41135ea2451984a79ed7061df499dd2ff
Keerthiprasanth <rkpkeerthi22@gmail.com>
2024-12-15 07:05:11 +0000

    Updated Navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="bg-customGreen text-white p-4 shadow-lg fixed w-full top-0 left-0 z-50">
@@ -21,6 +35,7 @@ function Navbar() {
         </button>
 
         <ul
+          ref={menuRef}
           className={`${
             isMenuOpen ? "flex" : "hidden"
           } md:flex md:flex-row md:space-x-6 md:items-center absolute md:static bg-customGreen w-full md:w-auto top-16 left-0 p-4 md:p-0 space-y-4 md:space-y-0 transition-all duration-300 ease-in-out`}
@@ -29,6 +44,7 @@ function Navbar() {
             <a
               href="#home"
               className="block bg-white md:px-4 rounded-sm text-customGreen hover:bg-customGreen hover:text-white transition duration-300"
+              onClick={() => setIsMenuOpen(false)}
             >
               Home
             </a>
@@ -37,6 +53,7 @@ function Navbar() {
             <a
               href="#about"
               className="block bg-white md:px-4 rounded-sm text-customGreen hover:bg-customGreen hover:text-white transition duration-300"
+              onClick={() => setIsMenuOpen(false)}
             >
               Details
             </a>
@@ -45,6 +62,7 @@ function Navbar() {
             <a
               href="#footer"
               className="block bg-white md:px-4 rounded-sm text-customGreen hover:bg-customGreen hover:text-white transition duration-300"
+              onClick={() => setIsMenuOpen(false)}
             >
               Contact
             </a>
